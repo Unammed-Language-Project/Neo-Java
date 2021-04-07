87a18079afdd4e147a8517f94d3a1514ad90213f
Yeff <76024824+YeffyCodeGit@users.noreply.github.com>
2021-04-07 16:31:51 +0600

    Made the repl be able to interpret things

---
+++ src/io/github/yeffycodegit/Expr.java
@@ -0,0 +1,68 @@
+package io.github.yeffycodegit;
+
+import io.github.yeffycodegit.Y.Token;
+
+public abstract class Expr {
+  public interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+    R visitGroupingExpr(Grouping expr);
+    R visitLiteralExpr(Literal expr);
+    R visitUnaryExpr(Unary expr);
+  }
+  public static class Binary extends Expr {
+    public Binary(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
+    }
+
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBinaryExpr(this);
+    }
+
+    public final Expr left;
+    public final Token operator;
+    public final Expr right;
+  }
+  public static class Grouping extends Expr {
+    public Grouping(Expr expression) {
+      this.expression = expression;
+    }
+
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
+      return visitor.visitGroupingExpr(this);
+    }
+
+    public final Expr expression;
+  }
+  public static class Literal extends Expr {
+    public Literal(Object value) {
+      this.value = value;
+    }
+
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLiteralExpr(this);
+    }
+
+    public final Object value;
+  }
+  public static class Unary extends Expr {
+    public Unary(Token operator, Expr right) {
+      this.operator = operator;
+      this.right = right;
+    }
+
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
+      return visitor.visitUnaryExpr(this);
+    }
+
+    public final Token operator;
+    public final Expr right;
+  }
+
+  public abstract <R> R accept(Visitor<R> visitor);
+}
