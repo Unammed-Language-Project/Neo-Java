312a88f10135af9d1c18e8132199ebe9f801a5b3
Yeff <76024824+YeffyCodeGit@users.noreply.github.com>
2021-04-27 15:40:30 +0600

    Added for loops, while loops, and if statements

--- src/io/github/yeffycodegit/Y/Interpreter.java
+++ src/io/github/yeffycodegit/Y/Interpreter.java
@@ -168,6 +168,10 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitWhileStmt(Stmt.While stmt) {
+        while (isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.body);
+        }
+
         return null;
     }
 
--- src/io/github/yeffycodegit/Y/Parser.java
+++ src/io/github/yeffycodegit/Y/Parser.java
@@ -1,6 +1,7 @@
 package io.github.yeffycodegit.Y;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static io.github.yeffycodegit.Y.TokenTypes.*;
@@ -186,6 +187,7 @@ public class Parser {
     }
 
     private Stmt statement() {
+        if (match(FOR)) return forStatement();
         if (match(IF)) return ifStatement();
         if (match(PRINT)) return printStatement();
         if (match(WHILE)) return whileStatement();
@@ -227,6 +229,51 @@ public class Parser {
         Stmt body = statement();
 
         return new Stmt.While(condition, body);
+    }
+
+    private Stmt forStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'for' statement.");
+
+        Stmt initializer;
+
+        if (match(SEMICOLON)) {
+            initializer = null;
+        } else if (match(VAR)) {
+            initializer = varDeclaration();
+        } else {
+            initializer = expressionStatement();
+        }
+
+        Expr condition = null;
+
+        if (!check(SEMICOLON)) {
+            condition = expression();
+        }
+        consume(SEMICOLON, "Expect ';' after loop condition.");
+
+        Expr increment = null;
+
+        if (!check(RIGHT_PAREN)) {
+            increment = expression();
+        }
+        consume(RIGHT_PAREN, "Expect ')' after for clauses.");
+
+        Stmt body = statement();
+
+        if (increment != null) {
+            body = new Stmt.Block(Arrays.asList(body, new Stmt.Expression(increment)));
+        }
+
+        if (condition == null) condition = new Expr.Literal(true);
+        body = new Stmt.While(condition, body);
+
+        if (initializer != null) {
+            body = new Stmt.Block(Arrays.asList(initializer, body));
+        }
+
+
+
+        return body;
     }
 
     List<Stmt> parse() {
